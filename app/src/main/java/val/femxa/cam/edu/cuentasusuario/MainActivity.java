5a9db3eca3049904229ba656fcd3de804e63a38e
SteveR98 <alvarodava@gmail.com>
2017-03-10 21:21:15 +0100

    CuentaUsuario

--- app/src/main/java/val/femxa/cam/edu/cuentasusuario/MainActivity.java
+++ app/src/main/java/val/femxa/cam/edu/cuentasusuario/MainActivity.java
@@ -3,16 +3,57 @@ package val.femxa.cam.edu.cuentasusuario;
 import android.Manifest;
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
 {
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+       Log.d("MENSAJE","El usuaio le ha dado a salir");
+
+        AlertDialog alertDialog= new AlertDialog.Builder(this).create();
+
+        alertDialog.setTitle("SALIR");
+        alertDialog.setMessage("¿De verdad quieres salir?");
+
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("MENSAJE","El usuaio confirma la salida");
+                finish();
+            }
+        });
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("MENSAJE","El usuaio confirma no quiere salir");
+                dialog.cancel();
+            }
+        });
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "NO SE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("MENSAJE","El usuaio no sabe");
+                dialog.cancel();
+            }
+        });
+
+        alertDialog.show();
+    }
+
     private void obtenerCuentas()
     {
 
@@ -62,9 +103,39 @@ public class MainActivity extends AppCompatActivity
         pedirPermisos();
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+
+        menu.add(Menu.NONE,1,1,"SALIR");
+        menu.add(Menu.NONE,2,2,"FEMXA");
 
 
 
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        Log.d("MENSAJE","Item ID: "+item.getItemId());
+
+        if (item.getItemId()==1)
+        {
+            this.finish();
+        }
+
+        else
+
+        {
+            Intent intent=new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("http://femxa-ebtm.rhcloud.com/html/home.html"));
+            startActivity(intent);
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
